596112c6dda00f571a1f6f33dc5d9eaf3ba3de69
Eric Hurst <eric.hurst@teem.com>
2017-03-30 14:51:28 -0600

    Version Bump 0.1.7

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@
 from setuptools import setup, find_packages
 setup(
         name="gavl",
-        version="0.1.6",
+        version="0.1.7",
         description="Data definition language and aggregation layer",
         packages=find_packages(),
         include_package_data=True,
